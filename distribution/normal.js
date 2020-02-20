804f656d541b34ba8c5c9f4525db132030138e41
Andrew Aaron <aaaron909@gmail.com>
2020-02-20 23:08:07 +1100

    Updated version of normal distribution script.

--- distribution/normal.js
+++ distribution/normal.js
@@ -1,17 +1,23 @@
-var values = [];
-var size = 50;
-var sum_amount = 12;
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
+// This demonstrates the Normal Distribution using random numbers from Math.random().                       //
+// It uses a histogram, which tallies the frequency of numbers. So if a '4' happens, 4 gets marked with 1.  //
+// If it happens again, '4' accumulates to 2 and so on.                                                     //
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+var values = [];                            // An array to store our histogram.
+var size = 50;                              // The amount of values in our histogram.
+var sum_amount = 12;                        // Amount of random numbers to sum before it's tallied inside the histogram.
 
 for (var i = 0; i < size; i++) {            // Initialize the histogram to 0.
     values[i] = 0;
 }
 
-for (var r = 0; r < 10; r++) {              // 20 so the log isn't so tall. 
+for (var r = 0; r < 5; r++) {               // 5 so the log isn't so tall. 
     for (var i = 0; i < size; i++) {
-        var choice = 0;
+        var choice = 0;                     // Which number should we pick? (0 - size)
 
         for (var j = 0; j < sum_amount; j++) {
-            choice += parseInt(Math.random() * size);       // Addition of 12 random numbers.
+            choice += parseInt(Math.random() * size);       // Addition of random numbers.
         }
 
         choice = parseInt(choice / sum_amount);     // Division by 12 does not create a linear average, but the bell curve.  
@@ -20,7 +26,7 @@ for (var r = 0; r < 10; r++) {              // 20 so the log isn't so tall.
     }                                               // So, between 2 and 12, the shape progresses from a triangle to the bell curve.
 }
 
-var max = 0;                            // Even cuter console.log() visualizer.
+var max = 0;                                // Even cuter console.log() visualizer.
 
 for (var i = 0; i < size; i++) {
     if (values[i + 1] > max) {
@@ -28,14 +34,14 @@ for (var i = 0; i < size; i++) {
     }
 }
 
-for (var l = max; l >= 0; l--) {        // 'l' is for line.
+for (var l = max; l >= 0; l--) {            // 'l' is for line. Go backwards or the graph will be upside down!
     var string = '';
 
     for(var i = 0; i < size; i++) {
-        if (values[i] <= l) {
-            string = string.concat('   ');
+        if (values[i] <= l) {               // Is the value lower than the current line?  
+            string = string.concat('  ');   // No, put a space. 
         } else {
-            string = string.concat(' | ');
+            string = string.concat('| ');   // Yes, put a line.
         }
     }
 
