c103db3e38cf56e425f95a41637b26df2bfb73c8
Andrew Aaron <aaaron909@gmail.com>
2020-02-23 00:08:52 +1100

    Swapped the dimensions of the neighbours array in Tile to make more sense.

--- wfc-two.js
+++ wfc-two.js
@@ -3,24 +3,25 @@
 var colors = require('colors');                 // npm install colors
 var fn = require(__dirname + '/functions.js').functions;
 var input = [                                   // Define the input/source array.
-    [1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [1, 2, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
 ];
 
 
-var tiles = [];                                     // An Array for our tile definitions.
+var tiles = [];                                 // An Array for our tile definitions.
 var x_length = input.length;
 var y_length = input[0].length;
 var output = [];                                // Define the output/target array.
-
-for (var x = 0; x < x_length; x++) {
+var x_out_length = 20;
+var y_out_length = 20;
+for (var x = 0; x < x_out_length; x++) {
     output[x] = [];
 
-    for (var y = 0; y < y_length; y++) {
+    for (var y = 0; y < y_out_length; y++) {
         output[x][y] = -1;                      // -1 represents maximum entropy ie. a tile hasn't been chosen.
     }
 }
@@ -30,20 +31,22 @@ class Tile {                                    // The Tile class.
         this.x = pos.x;
         this.y = pos.y;
         this.v = value;
-        this.neighbour = [];        
+        this.neighbour = [];
         this.up = fn.wrap(input, pos.y + 1);
         this.right = fn.wrap(input, pos.x + 1);
         this.down = fn.wrap(input, pos.y - 1);
         this.left = fn.wrap(input, pos.x - 1);
 
-        this.neighbour[0] = [input[pos.x][this.up]];
-        this.neighbour[1] = [input[this.right][this.up]];
-        this.neighbour[2] = [input[this.right][pos.y]];
-        this.neighbour[3] = [input[this.right][this.down]];
-        this.neighbour[4] = [input[pos.x][this.down]];
-        this.neighbour[5] = [input[this.left][this.down]];
-        this.neighbour[6] = [input[this.left][pos.y]];
-        this.neighbour[7] = [input[this.left][this.up]];
+        this.neighbour[0] = [
+            input[pos.x][this.up],
+            input[this.right][this.up],
+            input[this.right][pos.y],
+            input[this.right][this.down],
+            input[pos.x][this.down],
+            input[this.left][this.down],
+            input[this.left][pos.y],
+            input[this.left][this.up]
+        ];
     }
 
     pushNeighbours(x, y) {
@@ -52,41 +55,50 @@ class Tile {                                    // The Tile class.
         var down = fn.wrap(input, y - 1);
         var left = fn.wrap(input, x - 1);
 
-        this.neighbour[0].push(input[x][up]);
-        this.neighbour[1].push(input[right][up]);
-        this.neighbour[2].push(input[right][y]);
-        this.neighbour[3].push(input[right][down]);
-        this.neighbour[4].push(input[x][down]);
-        this.neighbour[5].push(input[left][down]);
-        this.neighbour[6].push(input[left][y]);
-        this.neighbour[7].push(input[left][up]);
+        this.neighbour.push([
+            input[x][up],
+            input[right][up],
+            input[right][y],
+            input[right][down],
+            input[x][down],
+            input[left][down],
+            input[left][y],
+            input[left][up]
+        ]);
     }
 }
-
-var counter = 0;
 
 for (var x = 0; x < x_length; x++) {
     for (var y = 0; y < y_length; y++) {            // x, y is for searching the input. 
         var val = input[x][y];
 
-        if (tiles.length > 0 && fn.tileAlreadyExists(tiles, val)) {
-            for (var t in tiles) {
+        if (tiles.length > 0 && fn.tileAlreadyExists(tiles, val)) {     // If the tile already exists, find it in the tiles array and
+            for (var t in tiles) {                                      // add the neighbours of the current cell (val) we are looking at.
                 if (val == tiles[t].v) {
                     tiles[t].pushNeighbours(x, y);
                 }
             }
         } else {
-            tiles.push(new Tile({ "x": x, "y": y }, input[x][y]));
-        }
+            tiles.push(new Tile({ "x": x, "y": y }, input[x][y]));      // Otherwise it doesn't exist, so it must be created. 
+        }                                                               // As tiles.length is zero initially, the program is forced to at 
+    }                                                                   // least create one unique case before proceeding. 
+}
+
+// for (var t = 0; t < tiles.length; t++) {
+//     console.log(tiles[t]);
+// }
+
+for (var x = 0; x < 20; x++) {
+    var line = '';
+
+    for (var y = 0; y < 20; y++) {
+        var choice = Math.floor(Math.random() * tiles.length)
+
+        output[x][y] = tiles[choice];
+        line = line.concat("  " + output[x][y].v);
     }
+    console.log(line);
 }
-
-for (var t = 0; t < tiles.length; t++) {
-    console.log("T: " + tiles[t].v)
-    console.log(tiles[t]);
-}
-
-// console.table(t);
 
 // console.log(t.length);
 
